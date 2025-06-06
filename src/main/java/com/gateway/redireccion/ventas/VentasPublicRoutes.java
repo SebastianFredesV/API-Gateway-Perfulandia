ce371112350d70dc1c4ba2753d499002074fc0b8
Sebastian Fredes <s.fredes@duocuc.cl>
2025-06-05 23:36:26 -0400

    Añadida carpeta y conexión con API Ventas

---
+++ src/main/java/com/gateway/redireccion/ventas/VentasPublicRoutes.java
@@ -0,0 +1,8 @@
+package com.gateway.redireccion.ventas;
+
+public class VentasPublicRoutes {
+        public static final String[] VENTAS_PUBLIC_GET = {
+        "/api/proxy/ventas",
+        "/api/proxy/ventas/"
+    };
+}

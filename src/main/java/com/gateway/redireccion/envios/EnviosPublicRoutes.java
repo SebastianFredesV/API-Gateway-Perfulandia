f1736621b2875436b0e0bb1cdb0ec5c312001112
Valeria Gallardo <va.gallardol@duocuc.cl>
2025-06-09 04:15:42 -0400

    Integración apis Vendedor y Envios (ProxyController) en redirección

---
+++ src/main/java/com/gateway/redireccion/envios/EnviosPublicRoutes.java
@@ -0,0 +1,5 @@
+package com.gateway.redireccion.envios;
+
+public class EnviosPublicRoutes {
+
+}
---
+++ src/main/java/com/gateway/redireccion/vendedores/VendedoresPublicRoutes.java
@@ -0,0 +1,5 @@
+package com.gateway.redireccion.vendedores;
+
+public class VendedoresPublicRoutes {
+
+}
